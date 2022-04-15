7d1c5656701222e95b267c0bc4410c7917cca442
Mirakwai Farrell <P2573296@my365.dmu.ac.uk>
2022-04-15 16:58:39 +0100

    Collection classes (properties) completed & tested

---
+++ ClassLibrary/clsOrderCollection.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ClassLibrary
+{
+    public class clsOrderCollection
+    {
+        //private data member for the list
+        List<clsOrder> mOrderList = new List<clsOrder>();
+
+        //public property for the order list 
+        public List<clsOrder> OrderList
+        {
+            get
+            {
+                //return the private data
+                return mOrderList;
+            }
+            set
+            {
+                //set the private data
+                mOrderList = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                //return the count of the list
+                return mOrderList.Count;
+            }
+            set
+            {
+                //later
+            }
+        }
+            
+
+        public clsOrder ThisOrder { get; set; }
+
+
+
+       /* public clsOrderCollection()
+        {
+            //create the items of the test data 
+            clsOrder TestItem = new clsOrder();
+            //set its properties
+            TestItem.OrderStatus = true;
+            TestItem.OrderId = 1;
+            TestItem.UserId = 10;
+            TestItem.OrderLine = 100;
+            TestItem.OrderDate = DateTime.Now.Date;
+            TestItem.OrderContent = "Ramen";
+            //add the item to the test list
+            mOrderList.Add(TestItem);
+            //re-initialise the object for some new data
+            TestItem = new clsOrder();
+            //set its properties
+            TestItem.OrderStatus = true;
+            TestItem.OrderId = 2;
+            TestItem.UserId = 21;
+            TestItem.OrderLine = 200;
+            TestItem.OrderDate = DateTime.Now.Date;
+            TestItem.OrderContent = "Rice";
+            mOrderList.Add(TestItem);
+        }*/
+
+        public clsOrderCollection()
+        {
+            //var for the index 
+            Int32 Index = 0;
+            //var to store the record count
+            Int32 RecordCount = 0;
+            //object for the data connection 
+            clsDataConnection DB = new clsDataConnection();
+            //execute the stored procedure 
+            DB.Execute("sproc_tblOrder_SelectAll");
+            //get the count of records
+            RecordCount = DB.Count;
+            //while there are records to process
+            while (Index < RecordCount)
+            {
+                //create blank order 
+                clsOrder AnOrder = new clsOrder();
+                //read in the fields from the current record
+                AnOrder.OrderStatus = Convert.ToBoolean(DB.DataTable.Rows[Index]["OrderStatus"]);
+                AnOrder.OrderId = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderId"]);
+                AnOrder.UserId = Convert.ToInt32(DB.DataTable.Rows[Index]["UserId"]);
+                AnOrder.OrderLine = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderLine"]);
+                AnOrder.OrderDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["OrderDate"]);
+                AnOrder.OrderContent = Convert.ToString(DB.DataTable.Rows[Index]["OrderContent"]);
+                //add the record to the private data member
+                mOrderList.Add(AnOrder);
+                //point at the next record
+                Index++;
+            }
+        }
+    }
+
+
+    
+}
---
+++ Testing4/tstOrderCollection.cs
@@ -0,0 +1,116 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instacne of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllOrders);
+        }
+
+        [TestMethod]
+        public void OrderListOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsOrder> TestList = new List<clsOrder>();
+            //add an item to the list
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //set its properties
+            TestItem.OrderStatus = true;
+            TestItem.OrderId = 1;
+            TestItem.UserId = 10;
+            TestItem.OrderLine = 100;
+            TestItem.OrderDate = DateTime.Now.Date;
+            TestItem.OrderContent = "Ramen";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllOrders.OrderList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.OrderList, TestList);
+        }
+
+        /*[TestMethod] //removed test
+        public void CountPropertyOK()
+        {
+            //create an instance of the class we cant to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            Int32 SomeCount = 2;
+            AllOrders.Count = SomeCount;
+            Assert.AreEqual(AllOrders.Count, SomeCount);
+        }*/
+
+        [TestMethod]
+        public void ThisOrderPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            clsOrder TestOrder = new clsOrder();
+            //set its properties of the test object
+            TestOrder.OrderStatus = true;
+            TestOrder.OrderId = 1;
+            TestOrder.UserId = 10;
+            TestOrder.OrderLine = 100;
+            TestOrder.OrderDate = DateTime.Now.Date;
+            TestOrder.OrderContent = "Ramen";
+            //assign the data to the property
+            AllOrders.ThisOrder = TestOrder;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.ThisOrder, TestOrder);
+        }
+
+        [TestMethod]
+        public void ListAndCountOk()
+        {
+            //create an instance of the class we want to create
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //create some test data to assign to the property
+            //in this case the data needs to be a list of objects
+            List<clsOrder> TestList = new List<clsOrder>();
+            //add an item to the list
+            //create the item of test data
+            clsOrder TestItem = new clsOrder();
+            //set its properties
+            TestItem.OrderStatus = true;
+            TestItem.OrderId = 1;
+            TestItem.UserId = 10;
+            TestItem.OrderLine = 100;
+            TestItem.OrderDate = DateTime.Now.Date;
+            TestItem.OrderContent = "Ramen";
+            //add the item to the test list
+            TestList.Add(TestItem);
+            //assign the data to the property
+            AllOrders.OrderList = TestList;
+            //test to see that the two values are the same
+            Assert.AreEqual(AllOrders.Count, TestList.Count);
+        }
+
+        /*[TestMethod] // removed test
+        public void TwoRecordsPresent()
+        {
+            //create an instacne of the class we want to create 
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            //test to see if that two values are the same
+            Assert.AreEqual(AllOrders.Count, 2);
+        }*/ 
+    }
+
+    
+  
+}
